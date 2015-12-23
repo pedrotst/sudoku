51c7c889c7fee2779b5fc3ac95a1a1d4615dca82
pedroabreu <x01205375163@E-098567.tcu.gov.br>
2015-12-23 12:17:00 -0200

    Adicionando board

--- board.py
+++ board.py
@@ -19,3 +19,44 @@ class Board(object):
 
 	def __repr__(self):
 		return self.__str__()
+
+	def set_element(self, elem, row, col):
+		self.board[row][col] = elem
+
+	def get_row(self, row):
+		return self.board[row]
+
+	def get_col(self, col):
+		return [x[col] for x in self.board]
+
+	def get_block(self, block):
+		block_list = []
+		if(block == 0):
+			for not_merged in [x[:3] for x in self.board[:3]]:
+				block_list += not_merged
+		elif(block == 1):
+			for not_merged in [x[3:6] for x in self.board[:3]]:
+				block_list += not_merged
+		elif(block == 2):
+			for not_merged in [x[6:9] for x in self.board[:3]]:
+				block_list += not_merged
+		elif(block == 3):
+			for not_merged in [x[:3] for x in self.board[3:6]]:
+				block_list += not_merged
+		elif(block == 4):
+			for not_merged in [x[3:6] for x in self.board[3:6]]:
+				block_list += not_merged
+		elif(block == 5):
+			for not_merged in [x[6:9] for x in self.board[3:6]]:
+				block_list += not_merged
+		elif(block == 6):
+			for not_merged in [x[:3] for x in self.board[6:9]]:
+				block_list += not_merged
+		elif(block == 7):
+			for not_merged in [x[3:6] for x in self.board[6:9]]:
+				block_list += not_merged
+		elif(block == 8):
+			for not_merged in [x[6:9] for x in self.board[6:9]]:
+				block_list += not_merged
+
+		return block_list
--- main.py
+++ main.py
@@ -1,6 +1,49 @@
 from board import Board
 
+def check_row(board):
+
+	print(board.get_row(2))
+
+def check_col(board):
+	print(board.get_col(1))
+
+def check_block(board):
+	print(board.get_block(8))
+
 if __name__ == '__main__':
 	board = Board()
+	board.set_element('2', 0, 1)
+	board.set_element('5', 1, 1)
+	board.set_element('4', 2, 1)
+	board.set_element('7', 1, 5)
+	board.set_element('6', 2, 4)
+	board.set_element('8', 2, 5)
+	board.set_element('1', 0, 8)
+	board.set_element('5', 2, 8)
+	board.set_element('7', 3, 1)
+	board.set_element('1', 3, 6)
+	board.set_element('3', 3, 8)
+	board.set_element('9', 4, 2)
+	board.set_element('7', 4, 3)
+	board.set_element('4', 5, 0)
+	board.set_element('3', 5, 3)
+	board.set_element('8', 5, 4)
+	board.set_element('7', 5, 7)
+	board.set_element('9', 5, 8)
+	board.set_element('6', 6, 0)
+	board.set_element('1', 6, 3)
+	board.set_element('9', 6, 8)
+	board.set_element('3', 7, 0)
+	board.set_element('2', 7, 2)
+	board.set_element('7', 7, 4)
+	board.set_element('5', 7, 5)
+	board.set_element('6', 7, 6)
+	board.set_element('4', 8, 2)
+	board.set_element('3', 8, 5)
 	print(board)
+	flag = True
+
+	while(flag):
+		flag = check_board
+	check_block(board)
 
